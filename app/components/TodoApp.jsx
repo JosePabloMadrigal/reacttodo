accac7341a4319fbc01d5ad9841c4fc930703e4a
Jose Pablo Madrigal <jose.pablo.madrigal@firstfactory.com>
2016-09-11 15:27:47 -0600

    Last updated

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,31 @@
 var React = require('react');
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'walk the dog'
+        }, {
+          id: 2,
+          text: 'Clean yard'
+        }, {
+          id: 3,
+          text: 'Play lol'
+        }, {
+          id: 4,
+          text: 'Go to the box'
+        }
+      ]
+    }
+  },
   render: function () {
+    var {todos} = this.state;
     return (
     <div>
-      TodoApp
+      <TodoList todos = {todos}/>
     </div>
     )
   }
